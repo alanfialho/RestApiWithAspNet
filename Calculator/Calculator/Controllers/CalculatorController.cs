090425962e4d4935736c4959bd99064ea789cf6c
alan.fialho <alan-1503@hotmail.com>
2019-02-21 07:51:07 -0300

    Implementing rest calculator

---
+++ Calculator/Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {   
+        // GET api/values/5
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public IActionResult Get(int firstNumber, int secondNumber)
+        {
+            return Ok(firstNumber + secondNumber);
+        }
+    }
+}
