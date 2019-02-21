d257cff4def49ec41ddef37ddb924038d3a96725
alan.fialho <alan-1503@hotmail.com>
2019-02-21 07:17:57 -0300

    redoing commit inicial

--- Scafolding/Scafolding/Controllers/ValuesController.cs
+++ Scafolding/Scafolding/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace RestApiWithAspNet.Controllers
+namespace Scafolding.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
