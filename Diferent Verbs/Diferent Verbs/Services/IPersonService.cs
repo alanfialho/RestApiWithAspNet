74bd9052d1fb0294371dfe598a58f92929ad1c39
alan.fialho <alan-1503@hotmail.com>
2019-02-26 13:51:49 -0300

    Creating person service

---
+++ Diferent Verbs/Diferent Verbs/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using Diferent_Verbs.Model;
+using System.Collections.Generic;
+
+namespace Diferent_Verbs.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        IList<Person> FindAll();
+        void Delete(long id);
+        Person Update(Person person);
+    }
+}
---
+++ Diferent Verbs/Diferent Verbs/Services/Implementations/PersonService.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Diferent_Verbs.Model;
+
+namespace Diferent_Verbs.Services.Implementations
+{
+    public class PersonService : IPersonService
+    {
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public IList<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+
+            for( int i = 0; i <= 8; i++)
+            {
+                persons.Add(FindById(i));
+            }
+
+            return persons;
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                ID = id,
+                FirstName = "FistName: " + id,
+                LastName = "LastName: " + id,
+                Gender = "Gender: " + id,
+                Address = "Address: " + id
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+    }
+}
