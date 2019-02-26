52a979a254392d9ffe3a193a5d5aca436b290bac
alan.fialho <alan-1503@hotmail.com>
2019-02-26 13:29:46 -0300

    DiferentVerbs initial commit

---
+++ Diferent Verbs/Diferent Verbs/Model/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace Diferent_Verbs.Model
+{
+    public class Person
+    {
+        public long ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+    }
+}
